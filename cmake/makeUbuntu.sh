d8fc68616f971ab0b57b5c0e70d4c664c874646f
Codi McKee <cmckee@tamu.edu>
2023-07-17 10:50:41 -0500

    Update makeUbuntu.sh

--- cmake/makeUbuntu.sh
+++ cmake/makeUbuntu.sh
@@ -3,7 +3,6 @@ cd ..
 mkdir build                                                            
 cd build                                                               
 /home/ubuntu/.local/bin/conan install .. --build missing
-mv /usr/lib/x86_64-linux-gnu/libpython3.10.a /usr/lib/x86_64-linux-gnu/libpython3.10.a.old
 cmake .. -DMUMPS_DIR=$PWD/../../mumps/build -DOPENMPI=TRUE -DSCALAPACK_LIBRARIES="/usr/lib/x86_64-linux-gnu/libmkl_blacs_openmpi_lp64.so;/usr/lib/x86_64-linux-gnu/libscalapack-openmpi.so.2.1"                                      
 cmake --build . --config Release --target OpenSees --parallel 4        
 cmake --build . --config Release --target OpenSeesPy                   
